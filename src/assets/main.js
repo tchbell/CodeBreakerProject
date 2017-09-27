8d7053a6e82355e215976077e46a568d60ea328f
Ted Bell <tchbell@gmail.com>
2017-09-27 16:35:44 -0400

    Fixed return bug in function

--- src/assets/main.js
+++ src/assets/main.js
@@ -14,7 +14,7 @@ function guess() {
 //implement new functions here
 let setHiddenFields = function (){
     attempt = 0;
-    answer = return Math.floor(Math.random() * (9999 - 0 +1)) + 0;
+    answer = Math.floor(Math.random() * (9999 - 0 +1)) + 0;
     answer = answer.toString();
     while(answer.length <= 3){
         answer = "0" + answer;
